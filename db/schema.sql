a65e307f271970ddd6abe23ca7f4c943ff64d0a3
Shady Coder <shadysaleh01@gmail.com>
2020-09-25 03:27:25 -0700

    Finished add role & role table functions

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE employee_tracker;
 CREATE TABLE department
 (
    id INT
-   UNSIGNED 
+
    AUTO_INCREMENT PRIMARY KEY,
    d_name VARCHAR
    (30)
@@ -15,44 +15,25 @@ CREATE TABLE department
    CREATE TABLE role
    (
       id INT
-      UNSIGNED 
       AUTO_INCREMENT PRIMARY KEY,
    title VARCHAR
       (30),
    salary DECIMAL
       (20,4),
-   department_id INT UNSIGNED ,
-     INDEX dep_ind
-      (department_id),
-  CONSTRAINT fk_department FOREIGN KEY
-      (department_id) REFERENCES department
-      (id) ON
-      DELETE CASCADE
+   department_id INT
 );
 
       CREATE TABLE employee
       (
          id INT
-         UNSIGNED 
+
          AUTO_INCREMENT PRIMARY KEY,
    first_name VARCHAR
          (30),
    last_name VARCHAR
          (30),
-   role_id INT UNSIGNED,
-     CONSTRAINT fk_role FOREIGN KEY
-         (role_id) REFERENCES role
-         (id) ON
-         DELETE CASCADE,
-    INDEX man_ind (manager_id),
-   manager_id INT
-         UNSIGNED,
-     INDEX role_ind
-         (role_id),
+   role_id INT ,
+     
+   manager_id INT ,
 
-  CONSTRAINT fk_manager FOREIGN KEY
-         (manager_id) REFERENCES employee
-         (id) ON
-         DELETE
-         SET NULL
          );
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql")
 const inquirer = require("inquirer")
 const fs = require("fs")
-const Choices = require("inquirer/lib/objects/choices")
+var Table = require('cli-table');
 // const consTable = require("console.table")
 // const util = require("util")
 
@@ -43,6 +43,7 @@ function start() {
             "Add employee",
             "Add Department",
             "Add Role",
+            "View Role",
             "Remove employee",
             "Update employee role",
             "Update employee manager"
@@ -68,6 +69,9 @@ function start() {
          case "Add Role":
             addRole()
             break;
+         case "View Role":
+            roleTable()
+            break;
          case "Remove employee":
             removeEmployee()
             break;
@@ -86,8 +90,11 @@ function byEmployee() {
    connection.query("SELECT * FROM employee", (err, res) => {
       if (err) throw err
       console.log("\n")
-      // console.log("id", "First_name", "Last_name", "Title", "Department", "Salary", "Manager")
+      console.log("id", "First_name", "Last_name", "Title", "Department", "Salary", "Manager")
+
+
       for (let i = 0; i < res.length; i++) {
+
          console.log("ID: " + res[i].id + "  ||  First Name: " + res[i].first_name + "  ||  Last Name: " + res[i].last_name + "  ||  Role: " + res[i].role_id + "  ||  Manager ID: " + res[i].manager_id)
       }
       console.log("\n")
@@ -96,7 +103,13 @@ function byEmployee() {
 
 }
 
-function byDepartment() { }
+function byDepartment() {
+   // connection.query("SELECT DISTINCT column, AGG_FUNC(column_or_expression), …
+   // FROM mytable
+   //     JOIN another_table
+   //       ON mytable.column = another_table.column
+   //     WHERE constraint_expression")
+}
 
 function byManager() { }
 
@@ -125,18 +138,23 @@ function addEmployee() {
          }
       },
       {
-         type: "input",
+         type: "list",
          name: "role",
-         message: "Please Enter The Role ID"
+         message: "What is The Employee's Role?",
+         choices: ["Salesperson", "Sales Lead", 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer']
       },
       {
-         type: "input",
+         type: "list",
          name: "manager",
-         message: "Please Enter The Manager ID"
+         message: "What is The Employee's Manager?",
+         choices: ['Saad', 'Stevie', 'Ahmed', "Mike", "None"]
       }
 
    ]).then((answer) => {
-      connection.query("INSERT INTO employee SET first_name = ?, last_name = ?, role_id = ?, manager_id = ?", [answer.first, answer.last, answer.role_id, answer.manager_id], (err, data) => {
+
+
+
+      connection.query("INSERT INTO employee SET first_name = ?, last_name = ?, role_id = ?, manager_id = ?", [answer.first, answer.last, answer.role, answer.manager], (err, data) => {
          if (err) throw err
          start()
       })
@@ -201,7 +219,7 @@ function addRole() {
       {
          type: "input",
          name: "department",
-         message: "Please Enter The Role's Department ID.",
+         message: "Please Enter The Role's Department Name.",
          validate: answer => {
             if (answer !== "") {
                return true;
@@ -210,18 +228,39 @@ function addRole() {
          }
       }
    ]).then((answer) => {
-      connection.query("INSERT INTO role SET title= ?, salary=?, department_id=?", [answer.title, answer.salary, answer.department], (err, res) => {
+
+      connection.query("SELECT id FROM department where d_name = ?", [answer.department], (err, res) => {
          if (err) throw err
+         connection.query("INSERT INTO role SET title= ?, salary=?, department_id=?", [answer.title, answer.salary, res[0].id], (err, res) => {
+            if (err) throw err
+         })
+         roleTable()
       })
-      roleTable()
+
    })
 }
 
 function roleTable() {
-   connection.query("SELECT title, salary, department_id FROM role ;", (err, res) => {
+   connection.query("SELECT role.id, role.title, role.salary, department.d_name FROM department JOIN role ON department.id = role.department_id;", (err, res) => {
       if (err) throw err
-      console.log(res)
+      console.log("\n")
+      for (let i = 0; i < res.length; i++) {
+
+         console.log("ID: " + res[i].id + "  ||  Title: " + res[i].title + "  ||  Salary: " + res[i].salary + "  ||  Dapartment: " + res[i].d_name)
+      }
+      console.log("\n")
       start()
+      // console.log(res)
+      // var table = new Table({
+      //    head: ['TH 1 label', 'TH 2 label']
+      //    , colWidths: [100, 200]
+      // });
+
+      // for (let i = 0; i < res.length; i++) {
+
+      //    table.push(res[i])
+      // }
+      // console.log(table.toString())
    })
 }
 
